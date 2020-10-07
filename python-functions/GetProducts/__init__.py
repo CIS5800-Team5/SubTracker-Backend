0afeecebc860b3bb0f5f18e801c705e2c6c02472
Jason Semon <jason.semon@nytimes.com>
2020-10-07 18:52:05 -0400

    created demo page

---
+++ python-functions/GetProducts/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import json
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    products = [
+        {
+            "name": "Azure DevOps",
+            "price": 4.99
+        },
+        {
+            "name": "Azure Functions",
+            "price": 0.01
+        }
+    ]
+
+    return func.HttpResponse(json.dumps(products))
